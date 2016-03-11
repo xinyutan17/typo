4f23292f9e7ba9d70dd7d56f6c73cb59ec8d42de
Xin Yu Tan <xinyutan@berkeley.edu>
2016-03-11 18:28:38 +0000

    undo changes

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -26,13 +26,11 @@ class Admin::CategoriesController < Admin::BaseController
   def new_or_edit
     @categories = Category.find(:all)
     if params[:id]
-      # edit
       @category = Category.find(params[:id])
-      @category.attributes = params[:category]
     else
-      # new
       @category = Category.new
     end
+    @category.attributes = params[:category]
     if request.post?
       respond_to do |format|
         format.html { save_category }
